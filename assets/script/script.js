350639084736dfcd2c4b63cd460f1f19435aae92
Felix ADEWALE <112468468+afunmibi@users.noreply.github.com>
2023-07-27 20:34:57 +0100

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,33 @@
-
-const menu = document.querySelector('.links');
 const hamburger = document.querySelector('.navlinks-mobile');
+const navLinks = document.querySelector('.links');
 
-hamburger.addEventListener('click', function(e) {
-  menu.classList.toggle('show');
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
 
-menu.addEventListener('click', function(e) {
-  if (e.target.tagName === 'A') {
-    menu.classList.remove('show');
-  }
-});
+// Contact us
+function validate(){
+    let fname = document.getElementById('fname');
+    let password = document.getElementById('password');
+    let email = document.getElementById('email');
+    
+        if(fname.value.trim() === ""){
+            alert('Enter your Full Name');
+            return false;
+            }
+    
+            else if(password.value.trim() ===""){
+                alert('Enter your password');
+                return false;
+            }
+    
+            else if(email.value.trim() ===""){
+                alert('Enter your email');
+                return false;
+            }
+            
+            else{
+                alert('Your message has been recorded')
+            }
+            }
+    
